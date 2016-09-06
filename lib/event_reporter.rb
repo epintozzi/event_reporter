4fbc7db48b1b547ca8abff89f264d32d556a1c66
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-09-05 20:57:40 -0600

    Add Person class and begin phone number sanitation method

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,4 +1,5 @@
 require "csv"
+require "./lib/Person"
 
 class EventReporter
 
@@ -6,16 +7,44 @@ class EventReporter
     zipcode.to_s.rjust(5, "0")[0..4]
   end
 
-  def load_file(filename = "event_attendees_short.csv")
+  def clean_phone(phone)
+    phone = phone.gsub(/\D/, "")
+    if phone.length > 11 or phone.length < 10
+      return "0000000000"
+    elsif phone.length == 11
+      if phone[0] == 1
+        phone = phone[1..11]
+      else
+        phone = "0000000000"
+      end
+    else
+      return phone
+    end
+  end
+
+  def load_file(filename = "./event_attendees_short.csv")
 
     contents = CSV.open filename, headers: true, header_converters: :symbol
 
-    # contents.each do |row|
-    #   id = row[0]
-    #   name = row[:first_name]
-    #   zipcode = clean_zipcode(row[:zipcode])
-    # end
+    contents.each do |row|
+      id = row[0]
+      reg_date = row[:regdate]
+      first_name = row[:first_name]
+      last_name = row[:last_name]
+      email_address = row[:email_address]
+      home_phone = clean_phone(row[:home_phone])
+      street = row[:street]
+      city = row[:city]
+      state = row[:state]
+      zipcode = clean_zipcode(row[:zipcode])
+    end
   end
+
+  def create_person
+
+
+  end
+
 
   def help
     "You can use the following commands: 'queue count', 'queue clear', 'queue district', 'queue print', 'queue print by', 'queue save to', 'queue export html', and 'find'."
